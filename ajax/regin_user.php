28e00fbf89b42d92644e511e54df44b900a47d0e
Gordeeva2006 <kgordeeva957@gmail.com>
2026-03-27 06:24:50 +0500

    Дописала проверку использовав библиотеку и изменив секретный ключ. Гордеева

--- ajax/regin_user.php
+++ ajax/regin_user.php
@@ -1,6 +1,7 @@
 <?php
 	session_start();
 	include("../settings/connect_datebase.php");
+	require_once("../libs/autoload.php");
 	
 	$login = $_POST['login'];
 	$password = $_POST['password'];
@@ -12,13 +13,28 @@
 	if($user_read = $query_user->fetch_row()) {
 		echo $id;
 	} else {
-		$mysqli->query("INSERT INTO `users`(`login`, `password`, `roll`) VALUES ('".$login."', '".$password."', 0)");
+		if(isset($_POST["g-recaptcha-response"]) == false) {
+			echo "Не прошли капчу";
+			exit;
+		}
+		$Secret = "6Ld_0pksAAAAAFHCw-3MYgjCQuLIij7ah1s1Q96v";
+		$Recaptcha = new \ReCaptcha\ReCaptcha($Secret);
+
+		$Response = $Recaptcha->verify($_POST["g-recaptcha-response"], $_SERVER['REMOTE_ADDR']);
+
+		if($Response->isSuccess()) {
+			$mysqli->query("INSERT INTO `users`(`login`, `password`, `roll`) VALUES ('".$login."', '".$password."', 0)");
 		
-		$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
-		$user_new = $query_user->fetch_row();
-		$id = $user_new[0];
+			$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
+			$user_new = $query_user->fetch_row();
+			$id = $user_new[0];
+
+			if($id != -1) $_SESSION['user'] = $id; 
+			echo $id;
+		} else {
+			echo "Пользователь не распознан";
+			exit;
+		}
 			
-		if($id != -1) $_SESSION['user'] = $id; // запоминаем пользователя
-		echo $id;
 	}
 ?>
